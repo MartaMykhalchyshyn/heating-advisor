b9b1b1a9f76d450b877358cf6763da5d83f40c91
ihordubas <ihordubas1525@gmail.com>
2021-11-30 20:54:59 +0200

    CTUI-6: Fix chart and search input

--- src/components/pages/signUp/signUp.jsx
+++ src/components/pages/signUp/signUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
-import { Link } from 'react-router-dom'
-import { Input, Button } from 'antd';
-import { UserOutlined, LockOutlined, EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import { Link } from "react-router-dom"
+import { Input, Button } from "antd"
+import { UserOutlined, LockOutlined, EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons"
 import http from "@utils/http"
 
 import "./signUp.sass"
@@ -12,36 +12,32 @@ const SignUp = () => {
     const [password, setPassword] = useState("")
     const [repeatedPassword, setRepeatedPassword] = useState("")
     const [message, setMessage] = useState("")
-    
-    const isValid = password != repeatedPassword
+
+    const isValid = password !== repeatedPassword
 
     const signUp = (email, password) => {
-        console.log('email', email)
-        console.log('password', password)
-
-        http.post('/auth/signup/', {'email': email, 'password': password })
+        http.post("/auth/signup/", { "email": email, "password": password })
             .then(response => {
-                response.status == 201 && setMessage(response.data.message)
+                response.status === 201 && setMessage(response.data.message)
                 console.log(response)
             })
             .catch(error => {
                 console.log(error.response.data.message)
                 setMessage(error.response.data.message)
-            })   
+            })
     }
-
 
     return (
         <div className="sign-up-page">
             <div className="sign-up-page-header">Sign up</div>
             <div className="sign-up-page-label">E-mail</div>
-            <Input 
+            <Input
                 onChange={e => setEmail(e.target.value)}
                 value={email}
-                placeholder="enter e-mail" 
+                placeholder="enter e-mail"
                 prefix={<UserOutlined />}
                 className="sign-up-page-item"
-                style={{height: '50px', fontSize: '20px'}}
+                style={{ height: "50px", fontSize: "20px" }}
             />
             <div className="sign-up-page-label">Password</div>
             <Input.Password
@@ -50,32 +46,32 @@ const SignUp = () => {
                 placeholder="enter password"
                 prefix={<LockOutlined />}
                 className="sign-up-page-item"
-                iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)} 
-                style={{height: '50px', fontSize: '20px'}}
+                iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                style={{ height: "50px", fontSize: "20px" }}
             />
             <div className="sign-up-page-label">Password</div>
-             <Input.Password
+            <Input.Password
                 onChange={e => setRepeatedPassword(e.target.value)}
                 value={repeatedPassword}
                 placeholder="repeat password"
                 prefix={<LockOutlined />}
                 className="sign-up-page-item"
-                iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)} 
-                style={{height: '50px', fontSize: '20px'}}
-                />
-            <Button 
-                onClick={() => signUp(email, password)} 
+                iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                style={{ height: "50px", fontSize: "20px" }}
+            />
+            <Button
+                onClick={() => signUp(email, password)}
                 disabled={isValid}
-                type="primary" 
-                size={'large'}
-                style={{width: '350px', fontSize: '25px', height: '50px'}}
-                >
+                type="primary"
+                size={"large"}
+                style={{ width: "350px", fontSize: "25px", height: "50px" }}
+            >
                 Sign up
             </Button>
             {message}
             <div className="sign-up-page-line"></div>
-            <div className="sign-up-page-link">Already have an account? 
-                <Link to="/signin" style={{marginLeft: '10px'}}>Sign in</Link>
+            <div className="sign-up-page-link">Already have an account?
+                <Link to="/signin" style={{ marginLeft: "10px" }}>Sign in</Link>
             </div>
         </div>
     )
